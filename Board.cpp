1c05a87634ec5a271ab80c17543c591fefe91535
LeChn <33189757+LeChn@users.noreply.github.com>
2017-12-01 15:55:59 -0500

    Add files via upload

---
+++ Board.cpp
@@ -0,0 +1,93 @@
+//******************************************************************************
+// Board.cpp
+// Revision History:
+//
+// Date           Author               Description
+// 11/28/2017     Jason Chen           Added class
+//******************************************************************************
+
+#include <iostream>
+#include "Deck.h"
+#include "Hand.h"
+#include "Card.h"
+#include "Board.h"
+#include "Player.h"
+using namespace std;
+
+//constructor
+//initialize the board for a new game
+Board::Board(Player hum)
+{
+	human = hum;
+	setCommunity();
+}
+
+void Board::setBlind(int bld)
+{
+	blind = bld;
+}
+
+void Board::setCommunity()
+{
+	for(int i = 0; i < 5; i++)
+	{
+		community[i] = dek.draw();
+	}
+}
+
+void Board::printBoard()
+{
+	cout << "The pot is: " << pot << endl;
+	cout << "Your stack size is: " << human.getTotalChips() << endl;
+	cout << "AI's stack size is: " << "Print AI's stack size" << endl << endl;
+}
+
+void Board::preflop()
+{
+	printBoard();
+	cout << endl << "Your hand: " << "Print Player One's hand" <<endl;
+	if(true) //player facing a bet
+	{
+		cout << "Fold, Check, or Bet" << endl;
+		//user input
+	}
+	else //player not facing a bet
+	{
+		cout << "Check or Bet" << endl;
+		//user input
+	}
+	//update pot
+	//update player's stack
+	//update AI's stack
+	printBoard();
+}
+
+void Board::flop()
+{
+	printBoard();
+	cout << "The flop is " << endl;
+	community[0].printCard();
+	community[1].printCard();
+	community[2].printCard();
+	cout << endl << "Your hand: " << "Print Player One's hand" <<endl;
+	if(true) //player facing a bet
+	{
+		cout << "Fold, Check, or Bet" << endl;
+		//user input
+	}
+	else //player not facing a bet
+	{
+		cout << "Check or Bet" << endl;
+		//user input
+	}
+	//update pot
+	//update player's stack
+	printBoard();
+
+}
+
+void Board::clearBoard()
+{
+	dek.shuffle();
+	setCommunity();
+}
