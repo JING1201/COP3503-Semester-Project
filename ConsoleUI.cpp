bd31db1ad27e1ef9738af328a8f472b981c646f6
kimko314 <33136566+kimko314@users.noreply.github.com>
2017-12-04 01:41:13 -0500

    ConsoleUI comments

--- ConsoleUI.cpp
+++ ConsoleUI.cpp
@@ -1,21 +1,37 @@
-/*
- * UI.cpp
- *
- *  Created on: Nov 27, 2017
- *      Author: lowji
- */
+//******************************************************************************
+//
+// File Name:     ConsoleUI.cpp
+//
+// File Overview: Controls the input and output of the poker game.	
 
 #include "ConsoleUI.h"
 
+//******************************************************************************
+// Function : constructor                                   
+// Process  : None                    
+// Notes    : None             
+//******************************************************************************    
 
 ConsoleUI::ConsoleUI() {
 	return;
 }
 
+//******************************************************************************
+// Function : welcome                                   
+// Process  : Outputs a welcome statement to the poker game.
+// Notes    : None
+//******************************************************************************    
+
 void ConsoleUI::welcome(){
 	cout<<"Welcome to the Poker Game!\n";
 	//can modify later for fancier display
 }
+
+//******************************************************************************
+// Function : input                                   
+// Process  : Outputs a string and returns a string.                
+// Notes    : None
+//******************************************************************************    
 
 string ConsoleUI::input(string request){
 	cout<<request;
@@ -23,6 +39,12 @@ string ConsoleUI::input(string request){
 	cin>>temp;
 	return temp;
 }
+
+//******************************************************************************
+// Function : constructor                                   
+// Process  : Outputs a string.
+// Notes    : None
+//******************************************************************************    
 
 void ConsoleUI::output(string content){
 	cout<<content<<endl;
