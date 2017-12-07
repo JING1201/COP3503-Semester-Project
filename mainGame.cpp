7b17aae0e400fa17ab182bb466070184b1363eee
jing1201 <low.jing@hotmail.com>
2017-12-06 21:26:24 -0500

    Fixed problem with negative input

--- mainGame.cpp
+++ mainGame.cpp
@@ -39,8 +39,8 @@ int main(){
 	AI->setTotalChips(stoi(inputTemp));
 
 	inputTemp=ui->input("How much do you want the small blind to be?");
-	while(!help->isInt(inputTemp)||stoi(inputTemp)>(human->getTotalChips()/20)){
-		ui->output("Invalid input.\nBlind must be an integer that is less than or equal to 5% of your chips.");
+	while(!help->isInt(inputTemp)||stoi(inputTemp)<=0||stoi(inputTemp)>(human->getTotalChips()/20)){
+		ui->output("Invalid input.\nBlind must be a positive integer that is less than or equal to 5% of your chips.");
 		inputTemp=ui->input("How much do you want the small blind to be?");
 	}
 
